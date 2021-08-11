0bdfd907087cf73096d0a22bed062dc4a88590dd
Alex Shander <65600346+AlexShander@users.noreply.github.com>
2021-08-11 10:54:18 +0600

    Update check_files.sh
    FIX brackets

--- monitor/check_files.sh
+++ monitor/check_files.sh
@@ -6,13 +6,10 @@ bot_id=$3
 bot_key=$4
 
 countfiles=$(find $mp3_path -type f -size +100k -cmin -20 -name *.mp3 -print | wc -l)
-
 if [ $countfiles -eq 0 ]
 then
-   curl -d -i -X GET  https://api.telegram.org/bot$bot_id:$bot_key/sendMessage \
+   curl -s -d -i -X GET  https://api.telegram.org/bot$bot_id:$bot_key/sendMessage \
    --data chat_id="$chat_id" \
    --data-urlencode text="Не собирается файлы mp3 с фикрофона по адресу $mp3_path
 Сервер: $HOSTNAME" > /dev/null
 fi
-
-
