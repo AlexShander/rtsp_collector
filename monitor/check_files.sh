9b517ea4245bedb4f3e86a6839819b7bf74032aa
Alexandr Shander <alexmasc05@gmail.com>
2021-08-11 10:04:24 +0600

    Add monitor script for cron. We will check to the new mp3 files  would exist.

---
+++ monitor/check_files.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+mp3_path=$1
+chat_id=$2
+bot_id=$3
+bot_key=$4
+
+countfiles=$(find ./ -type f -size 100k -cmin -20 -name *.mp3 -print | wc -l)
+
+if [ $countfiles -gt 0 ]
+then
+   curl -d -i -X GET  https://api.telegram.org/bot$bot_id:$bot_key/sendMessage \
+   --data chat_id="$chat_id" \
+   --data-urlencode text="Не собирается файлы mp3 с фикрофона по адресу $mp3_path
+Сервер: $HOSTNAME" > /dev/null
+fi
+
+
